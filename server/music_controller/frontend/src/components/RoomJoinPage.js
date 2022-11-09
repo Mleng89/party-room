de3e51d376002bb414b32e88ba806f86ef2bfcf4
Matthew Leng <60058291+Mleng89@users.noreply.github.com>
2022-11-09 17:51:53 -0500

    FRONTEND: src files created

---
+++ server/music_controller/frontend/src/components/RoomJoinPage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class RoomJoinPage extends Component {
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return <p>This is the room join page</p>;
+	}
+}
+
+// export default RoomJoinPage(){
+//     return(
+//  <p>This is the room join page</p>
+//     )
+// }
