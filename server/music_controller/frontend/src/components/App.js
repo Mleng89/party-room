7d30049354b391f34a87c36ac6772c7552f1bf36
Matthew Leng <60058291+Mleng89@users.noreply.github.com>
2022-11-09 18:14:28 -0500

    FRONTEND: React component edit for App.js

--- server/music_controller/frontend/src/components/App.js
+++ server/music_controller/frontend/src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { render } from 'react-dom';
-import HomePage from './HomePage';
+// import HomePage from './HomePage';
 
 export default class App extends Component {
 	constructor(props) {
@@ -10,7 +10,7 @@ export default class App extends Component {
 	render() {
 		return (
 			<div>
-				<HomePage />
+				<h1>TEST TEST</h1>
 			</div>
 		);
 	}
