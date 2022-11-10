a9ae84f6a9b1b671b0531aee018b2d34650dd625
Matthew Leng <60058291+Mleng89@users.noreply.github.com>
2022-11-09 22:01:15 -0500

    FRONTEND: Join & Create URLS working

--- server/music_controller/frontend/urls.py
+++ server/music_controller/frontend/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from .views import index
 
-urlpatterns = [path("", index)]
+urlpatterns = [
+    path("", index),
+    path("join", index),
+    path("create", index),
+]
