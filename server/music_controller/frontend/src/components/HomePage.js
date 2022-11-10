51433bf4206c1d5d3f5e63551fd49346f228d916
Matthew Leng <60058291+Mleng89@users.noreply.github.com>
2022-11-10 18:05:18 -0500

    FRONTEND: Room route created

--- server/music_controller/frontend/src/components/HomePage.js
+++ server/music_controller/frontend/src/components/HomePage.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import RoomJoinPage from './RoomJoinPage';
 import CreateRoomPage from './CreateRoomPage';
 import { BrowserRouter, Routes, Route, Link, Redirect } from 'react-router-dom';
+import Room from './Room';
 
 // export default class HomePage extends Component {
 // 	constructor(props) {
@@ -31,6 +32,7 @@ export default function HomePage() {
 					<Route path='/' element={<p>This is the Home Page</p>} />
 					<Route path='/join/*' element={<RoomJoinPage />} />
 					<Route path='/create' element={<CreateRoomPage />} />
+					<Route path='/room/:roomCode' element={<Room />} />
 				</Routes>
 			</BrowserRouter>
 		</div>
